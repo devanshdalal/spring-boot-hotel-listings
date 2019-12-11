e86b630ce09c77f7bf62db8ded5917f3ae183f7e
Devansh Dalal <devansh.dalal@thoughtspot.com>
2019-12-11 19:08:52 +0530

    Added local mongodb

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Start the local mongodb instance
+bin/mongod --dbpath data/ &
+
 mongo listings --eval "printjson(db.HotelListing.remove({}))"
 chmod +x ./csvcut
 ./csvcut -d ',' -q \" -f 2,4,5,6,7,8,9,10 data/listings.csv > data/extracted.csv
