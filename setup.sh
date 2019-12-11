bc01169f1d00c5e73224cd695acffbe6c6e00d46
Devansh Dalal <devansh.dalal@thoughtspot.com>
2019-12-11 19:16:54 +0530

    update setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cd bin
 # Start the local mongodb instance
-bin/mongod --dbpath data/ &
+./mongod --dbpath ../data/ &
 
-mongo listings --eval "printjson(db.HotelListing.remove({}))"
-chmod +x ./csvcut
-./csvcut -d ',' -q \" -f 2,4,5,6,7,8,9,10 data/listings.csv > data/extracted.csv
-mongoimport -d listings -c HotelListing --type csv --file data/extracted.csv --headerline --mode merge
+./mongo listings --eval "printjson(db.HotelListing.remove({}))"
+chmod +x ../csvcut
+../csvcut -d ',' -q \" -f 2,4,5,6,7,8,9,10 ../data/listings.csv > ../data/extracted.csv
+./mongoimport -d listings -c HotelListing --type csv --file ../data/extracted.csv --headerline --mode merge
